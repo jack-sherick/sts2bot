b4a9f57613e30a2d6bd1eac13c15ab2a7961c1bd
jack-sherick <jacksherick@gmail.com>
2026-03-23 17:27:40 -0700

    small change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const { Client, Events, GatewayIntentBits, AttachmentBuilder } = require('discord.js');
 
 require('dotenv').config();
-const token = process.env.WORKING_BRANCH_TOKEN;
+const token = process.env.DISCORD_TOKEN;
 
 const path = require("path");
 const cards = require("./lib/cards.json");
